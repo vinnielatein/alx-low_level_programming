413927bd73a5f769f26149a22d6b71d3bc45bbfd
mohamed <clyde_python@yahoo.com>
2023-07-16 10:50:31 -0700

    hein

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Description: print all single digit numbers
+ *
+ * Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+	char ch = 'z';
+
+	while (digit >= 'a')
+	{
+		putchar(ch);
+		ch--;
+	}
+	putchar("\n");
+
+	return (0);
+}
