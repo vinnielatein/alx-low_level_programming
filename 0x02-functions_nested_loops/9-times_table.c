82d6b2f9e6d45895b88793b9002062443c7bf575
mohamed <clyde_python@yahoo.com>
2023-07-19 15:51:36 -0700

    hempe

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,7 +13,7 @@ void times_table(void)
 {
 	int num, mult, prod;
 
-	for (num = 0; num <= 9; mult++)
+	for (num = 0; num <= 9; num++)
 	{
 		_putchar(48);
 		for (mult = 1; mult <= 9; mult++)
@@ -27,6 +27,7 @@ void times_table(void)
 				_putchar(' ');
 			else
 				_putchar((prod / 10) + 48);
+
 			_putchar((prod % 10) + 48);
 		}
 		_putchar('\n');
