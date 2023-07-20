3715956419083b68b528a59ec2f65e90b3f645f8
mohamed <clyde_python@yahoo.com>
2023-07-20 03:46:20 -0700

    kk

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - multiply numbers
+ *
+ * @a: input for first number
+ * @b: input for second number
+ *
+ * Return: return results
+*/
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints 0-9 only using _putchar twice
+ *
+ * Return: always 0 (success)
+*/
+
+int print_numbers(void)
+{
+	int num = 0;
+
+	do {
+		_putchar(num + 48);
+		num++;
+	} while (num >= 0 && num <= 9);
+	_putchar('\n')
+}
