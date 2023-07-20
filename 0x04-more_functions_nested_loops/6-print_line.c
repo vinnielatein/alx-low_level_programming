8c8800210c08e214aacb0344d33ad840f2f56b58
mohamed <clyde_python@yahoo.com>
2023-07-20 14:28:34 -0700

    hissssss

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_line - prints a straight line
+ *
+ * @n: is the number of times the _ char should be printed
+*/
+
+void print_line(int n)
+{
+	int lnChr;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (lnChr = 1; lnChr <= n; lnChr++)
+			_putchar('_');
+		_putchar('\n');
+	}
+}
