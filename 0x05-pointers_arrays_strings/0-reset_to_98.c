cc82bdf9eea573d251af6148735b69295891948b
mohamed <clyde_python@yahoo.com>
2023-07-25 10:42:05 -0700

    test

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * reset_to_98 - a function that takes a paramete into it and ypdate it to 98
+ *
+ * @n: int parameter
+ *
+ * Return: nothing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
