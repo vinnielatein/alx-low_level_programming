345897c5d66adc2dc896ca64f6735d89197a0380
mohamed <clyde_python@yahoo.com>
2023-07-31 14:04:06 -0700

    hello

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -9,12 +9,13 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	int x = 0;
+	int i = 0;
 
-	for (; n > 0; x++)
+	for (; n > 0; i++)
 	{
-		s[x] = b;
+		s[i] = b;
 		n--;
 	}
 	return (s);
 }
+
