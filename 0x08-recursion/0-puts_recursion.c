66af58cb20d8b9ecfa492b84fefce7c6c2866fe8
mohamed <clyde_python@yahoo.com>
2023-08-03 11:10:22 -0700

    si

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -4,14 +4,15 @@
  * @s: input
  * Return: Always 0 (Success)
  */
-
 void _puts_recursion(char *s)
 {
-	if (*s == 0)
+	if (*s)
 	{
-		_putchar('\n');
-		return;
+		_putchar(*s);
+		_puts_recursion(s + 1);
 	}
-	_putchar(*s);
-	_puts_recursion(s + 1);
+
+	else
+		_putchar('\n');
 }
+
