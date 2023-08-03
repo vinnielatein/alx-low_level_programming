2f3e0d87ff880f98f4070c19fd2fefa0a665c4ec
mohamed <clyde_python@yahoo.com>
2023-08-03 10:49:27 -0700

    system

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,23 +1,18 @@
 #include "main.h"
-
 /**
- * _puts_recursion - function that prints a string by recursion
- *
- * @s: input for a pointer on a string array
- *
- * Return: nothing
-*/
-
+ * _puts_recursion - function like puts();
+ * @s: input
+ * Return: Always 0 (Success)
+ */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		_putchar('\n');
-	}
-	else
+	if (*s)
 	{
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
 
+	else
+		_putchar('\n');
 }
+
