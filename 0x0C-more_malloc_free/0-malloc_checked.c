57d9fd4c1b5f524f53ff13d7291b41887ebdc12f
mohamed <clyde_python@yahoo.com>
2023-08-10 15:15:29 -0700

    heheheh

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - alloctes memory using malloc and exit if failed
+ * @b: int
+ * Return: pointer to the array intialized or null
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	int *m = malloc(b);
+
+	if (m == 0)
+		exit(98);
+
+	return (m);
+}
