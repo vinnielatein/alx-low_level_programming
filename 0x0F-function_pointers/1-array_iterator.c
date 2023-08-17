7cb5624f6f663ccad4defff92bcfea8f3c3d86c4
mohamed <clyde_python@yahoo.com>
2023-08-17 10:53:24 -0700

    jhjh

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,17 +1,21 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
- * array_iterator function that executes a function on paramter
- * @size: size of the array
- * @action: function pointer
- * @array: the int array
+ * array_iterator - prints each array elem on a newl
+ * @array: array
+ * @size: how many elem to print
+ * @action: pointer to print in regular or hex
  * Return: void
-*/
+ */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	int *end = array + size - 1;
+	unsigned int i;
 
-	if (array && size && action)
-		while (array <= end)
-			action(*array++);
+	if (array == NULL || action == NULL)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -1,22 +1,22 @@
-#include "functions_pointers.h"
-
+#include "function_pointers.h"
 /**
- * int_index  - searches for an integer
- * @size: the array size
- * @array: the int array
- * @cmp: the compare function
- * Return: the integer index
-*/
+ * int_index - return index place if comparison = true, else -1
+ * @array: array
+ * @size: size of elements in array
+ * @cmp: pointer to func of one of the 3 in main
+ * Return: 0
+ */
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int i = 0;
+	int i;
 
-	if(array && size && cmp)
-		while (i < size)
-		{
-			if (cmp(array[i]))
-				return (i);
-			i++;
-		}
-	return(-1);
+	if (array == NULL || size <= 0 || cmp == NULL)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]))
+			return (i);
+	}
+	return (-1);
 }
